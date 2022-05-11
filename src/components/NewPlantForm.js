021c0bf71f484ebd1edd9ebc0ecdfdf7a3520d3e
Lionel Mason <capitalcookhouse@gmail.com>
2022-05-11 17:17:31 -0400

    commit tree 6ce7442e409ce464ade2a8cf1087f4a56fcbf592

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,18 +1,26 @@
 import React, {useState} from "react";
 
-function NewPlantForm({}) {
+function NewPlantForm({createPlant}) {
 
-  // const [formData, setFormData] = useState({
-  //     name: "", 
-  //     image: "",
-  //     price: ""
-  //   })
+  const [formData, setFormData] = useState({
+      name: "", 
+      image: "",
+      price: ""
+  })
 
-  //   function handleChange(e) {
-  //     const updatedFormData = {...formData, [e.target.name]:e.target.value}
-  //     setFormData(updatedFormData)
+  const handleChange =(e) => {
+      const updatedFormData = {...formData, [e.target.name]:e.target.value}
+      setFormData(updatedFormData)
 
-  //   }
+  }
+
+//define handleSubmit(e.preventDefault) .. attach to <form>.. invoke createPlant ..use formData 
+
+  const handleSubmit= (e) => {
+    e.preventDefault();
+    createPlant(formData);
+  }
+
 
 
 
@@ -20,14 +28,14 @@ function NewPlantForm({}) {
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        {/* <input type="text" name="name" placeholder="Plant name" value={formData.name} onChange={handleChange} />
+      <form onSubmit={handleSubmit} >
+        <input type="text" name="name" placeholder="Plant name" value={formData.name} onChange={handleChange} />
         <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={handleChange} />
         <input type="number" name="price" step="0.01" placeholder="Price" value={formData.price} onChange={handleChange} />
-        <button type="submit">Add Plant</button> */}
+        <button type="submit">Add Plant</button>
       </form>
     </div>
-  );
+  )
 }
 
 export default NewPlantForm;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,5 +1,4 @@
-import { create } from "json-server";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
@@ -8,25 +7,55 @@ import Search from "./Search";
 
 function PlantPage() {
  
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("")
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(plants => setPlants(plants));
+  },[])
+  
+  
+  const createPlant = (plantObj) => {
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(plantObj)
+    })
+    .then((r) => r.json())
+    .then((newPlant) => setPlants(previousPlants => [...previousPlants, newPlant]))
+  }
 
+  const displayedPlants = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(searchTerm.toLowerCase());
+  })
 
-  // function createPlant(plantObj) {
-  //   fetch("http://localhost:6001/plants", {
-  //     method: "POST",
-  //     header: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(plantObj)
-  //   })
-  //   .then((r) => r.json())
-  //   .then(() => ()
-  // }
+  const handleDeletePlant = (id) => {
+    const updatedPlantArr = plants.filter((plant) => plant.id !== id);
+    setPlants(updatedPlantArr)
+  }
+
+  const handleUpdatedPlant = (updatedPlant) => {
+    const updatedPlantArr = plants.map((plant) => {
+      if (plant.id === updatedPlant.id) {
+        return updatedPlant;
+      } else {
+        return plant;
+      }
+    });
+    setPlants(updatedPlantArr)
+  }
+
 
   return (
     <main>
-      <NewPlantForm  />
-      <Search />
-      <PlantList />
+      <NewPlantForm createPlant={createPlant}  />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}  />
+      <PlantList plants={displayedPlants} onDelete={handleDeletePlant} onUpdatePlant={handleUpdatedPlant} />
     </main>
   );
 }
